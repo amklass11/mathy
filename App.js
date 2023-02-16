0057faf4bc6fc919d6dcc85799a0625e2c0d17e7
amklass11 <lassanaa14@gmail.com>
2023-02-16 12:05:43 +0100

    fix

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
-import React, { memo } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Homepage from './components/Homepage';
-import Calpage from './components/Calpage';
-import Quotepage from './components/Quotepage';
+import React from 'react';
+import './App.css';
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from 'react-router-dom';
+import Home from './components/home';
+import Quote from './components/quote';
+import Calculator from './components/calculator';
 
-const App = memo(() => (
-  <Routes>
-    <Route path="/quotepage" element={<Quotepage />} />
-    <Route path="/calpage" element={<Calpage />} />
-    <Route path="/" element={<Homepage />} />
-  </Routes>
-));
-App.displayName = 'App';
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="header-container">
+          <h1>Math Magician</h1>
+          <nav>
+            <Link to="/">Home</Link>
+            <span> | </span>
+            <Link to="/calculator">Calculator</Link>
+            <span> | </span>
+            <Link to="/quote">Quote</Link>
+          </nav>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator previous="" operand="" current="0" solved={false} />} />
+          <Route path="/quote" element={<Quote />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    );
+  }
+}
 
 export default App;
