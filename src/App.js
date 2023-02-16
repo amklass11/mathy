87a46b4f641e622d16ec0174f602ec16128377d8
amklass11 <lassanaa14@gmail.com>
2023-02-16 10:38:31 +0100

    fix linters 4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import * as React from 'react';
+import React from 'react';
 import './App.css';
 import { Calculator } from './components/calculator';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
