1f4405f38a4ce6bcfb7a62b5d8195b1616c741a0
amklass11 <lassanaa14@gmail.com>
2023-02-16 11:28:59 +0100

    fix lint

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -2,7 +2,7 @@ import { React, useState } from 'react';
 import PropTypes from 'prop-types';
 import Buttons from './buttons';
 import Display from './display';
-import calculate from '../logic/calculate';
+import calculate from '../logic/calculate.js';
 import './calculator.css';
 
 const Calculator = (props) => {
