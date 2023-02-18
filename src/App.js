fe1f7173b9a03ab9fbc842d20cf76b162a3f11fd
amklass11 <lassanaa14@gmail.com>
2023-02-18 05:33:55 +0100

    go

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-// eslint-disable-next-line react/prefer-stateless-function
 function App() {
   return (
     <div className="App">
