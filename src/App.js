0f45783dea3d256863d4eee0f0a3d63394af5c5e
amklass11 <lassanaa14@gmail.com>
2023-02-18 05:22:26 +0100

    cahnged made

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
-import Calculator from './components/calculator';
+import Calculator from './components/Calculator';
 
 // eslint-disable-next-line react/prefer-stateless-function
-class App extends React.Component {
-  render() {
-    return (
-      (<Calculator previous="" operand="" current="0" solved={false} />)
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
 }
-
 export default App;
